4250651962db670e72d9d0b3d9da21f88aea898e
Kristof Buts <git@kristofbuts.be>
2025-03-16 16:09:25 +0100

    disable parallel running of integration tests

--- api/tests/Application.IntegrationTests/Common/ApplicationTestBase.cs
+++ api/tests/Application.IntegrationTests/Common/ApplicationTestBase.cs
@@ -1,14 +1,22 @@
+using TUnit.Core.Interfaces;
+
 namespace SplitTheBill.Application.IntegrationTests.Common;
 
+[ParallelLimiter<ApplicationTestBaseParallelLimiter>]
 internal abstract class ApplicationTestBase
 {
     [ClassDataSource<ApplicationFixture>(Shared = SharedType.PerTestSession)]
     public required ApplicationFixture Application { get; init; }
-    
+
     [Before(Test)]
     public async Task ResetStateAsync()
     {
         // reset the application state before every single test
         await Application.ResetStateAsync();
     }
+}
+
+internal sealed record ApplicationTestBaseParallelLimiter : IParallelLimit
+{
+    public int Limit => 1;
 }
