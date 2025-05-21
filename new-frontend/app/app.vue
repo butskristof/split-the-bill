fa9db87e08e307d29df37a0faf9cf68065bfaa8a
Kristof Buts <git@kristofbuts.be>
2025-05-21 13:18:02 +0200

    prime setup

--- new-frontend/app/app.vue
+++ new-frontend/app/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <PrimeButton label="Verify" />
   </div>
 </template>
--- new-frontend/nuxt.config.ts
+++ new-frontend/nuxt.config.ts
@@ -1,3 +1,5 @@
+import Aura from '@primeuix/themes/aura';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
@@ -5,8 +7,19 @@ export default defineNuxtConfig({
     compatibilityVersion: 4,
   },
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@primevue/nuxt-module'],
   components: {
     dirs: [],
+  },
+  primevue: {
+    autoImport: true,
+    components: {
+      prefix: 'Prime',
+    },
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
   },
 });
