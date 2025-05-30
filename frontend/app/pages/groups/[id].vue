b9cda40dab2e7d6e9725833197769184649483b9
Kristof Buts <git@kristofbuts.be>
2025-05-30 13:25:41 +0200

    load group data

--- frontend/app/pages/groups/[id].vue
+++ frontend/app/pages/groups/[id].vue
@@ -2,7 +2,15 @@
   <div class="group-detail-page">
     <AppPageBackButton :default-route="{ name: 'groups' }" />
     <AppPageMain>
-      <h1>{{ groupId }}</h1>
+      <LoadingIndicator v-if="status === 'pending'" />
+      <ApiError
+        v-if="error"
+        :error="error"
+      />
+      <template v-if="group">
+        <h1>{{ group.name }}</h1>
+        <PreformattedText :value="group" />
+      </template>
     </AppPageMain>
   </div>
 </template>
@@ -10,7 +18,16 @@
 <script setup lang="ts">
 import AppPageMain from '~/components/app/AppPageMain.vue';
 import AppPageBackButton from '~/components/common/AppPageBackButton.vue';
+import ApiError from '~/components/common/ApiError.vue';
+import LoadingIndicator from '~/components/common/LoadingIndicator.vue';
+import PreformattedText from '~/components/common/PreformattedText.vue';
 
 const route = useRoute();
 const groupId = route.params.id as string;
+const key = computed(() => `groups/${groupId}`);
+const {
+  data: group,
+  status,
+  error,
+} = await useLazyBackendApi('/Groups/{id}', { key, path: { id: groupId } });
 </script>
