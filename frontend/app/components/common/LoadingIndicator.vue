581d61bbb5feb594fa38700ef61bc0b50d3d6923
Kristof Buts <git@kristofbuts.be>
2025-05-30 11:53:29 +0200

    api error, loading indicator, fix color mode, autoclose dropdown

---
+++ frontend/app/components/common/LoadingIndicator.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="loading-indicator">
+    <i class="pi pi-spin pi-spinner" />
+    <div v-if="message">{{ message }}</div>
+    <div v-if="entity">Loading {{ entity }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  message?: string;
+  entity?: string;
+}>();
+</script>
+
+<style scoped lang="scss">
+@use '~/assets/styles/utilities.scss';
+
+.loading-indicator {
+  padding-block: calc(var(--default-spacing) * 4);
+  text-align: center;
+  @include utilities.flex-column(false);
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+
+  .pi {
+    font-size: 2rem;
+  }
+}
+</style>
--- frontend/app/pages/api-test.vue
+++ frontend/app/pages/api-test.vue
@@ -5,14 +5,28 @@
       label="refresh"
       @click="refresh"
     />
-    <PreformattedText :value="data" />
+    <LoadingIndicator
+      v-if="isPending"
+      entity="group"
+    />
+    <PreformattedText
+      v-if="data"
+      :value="data"
+    />
+    <ApiError
+      v-if="error"
+      :error="error"
+    />
   </AppPageMain>
 </template>
 
 <script setup lang="ts">
 import AppPageMain from '~/components/app/AppPageMain.vue';
 import PreformattedText from '~/components/common/PreformattedText.vue';
-const { data, refresh } = await useLazyBackendApi('/Members', {
+import ApiError from '~/components/common/ApiError.vue';
+import LoadingIndicator from '~/components/common/LoadingIndicator.vue';
+const { data, error, refresh, status } = await useLazyBackendApi('/Members', {
   key: 'members',
 });
+const isPending = computed(() => status.value === 'pending');
 </script>
