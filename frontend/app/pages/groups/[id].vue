e26b5be45e8a7f027d88e2413e56166ec8aa1d1b
Kristof Buts <git@kristofbuts.be>
2025-06-04 00:18:29 +0200

    set up detail page w/ cards

--- frontend/app/pages/groups/[id].vue
+++ frontend/app/pages/groups/[id].vue
@@ -1,31 +1,32 @@
 <template>
   <div class="group-detail-page">
     <AppPageBackButton :default-route="{ name: 'groups' }" />
-    <AppPageMain>
-      <PageLoadingIndicator v-if="status === 'pending'" />
-      <ApiError
-        v-if="error"
-        :error="error"
-      />
-      <template v-if="group">
+    <PageLoadingIndicator v-if="status === 'pending'" />
+    <ApiError
+      v-if="error"
+      :error="error"
+    />
+    <template v-if="group">
+      <main>
         <h1>{{ group.name }}</h1>
-        <GroupDetailRecentActivity :group="group" />
-        <PreformattedText
-          v-if="false"
-          :value="group"
-        />
-      </template>
-    </AppPageMain>
+        <AppPageCard>
+          <GroupDetailMembers :members="group.members!" />
+        </AppPageCard>
+        <AppPageCard>
+          <GroupDetailRecentActivity :group="group" />
+        </AppPageCard>
+      </main>
+    </template>
   </div>
 </template>
 
 <script setup lang="ts">
-import AppPageMain from '~/components/app/AppPageMain.vue';
 import AppPageBackButton from '~/components/common/AppPageBackButton.vue';
 import ApiError from '~/components/common/ApiError.vue';
-import PreformattedText from '~/components/common/PreformattedText.vue';
 import GroupDetailRecentActivity from '~/components/groups/detail/GroupDetailRecentActivity.vue';
 import PageLoadingIndicator from '~/components/common/PageLoadingIndicator.vue';
+import AppPageCard from '~/components/app/AppPageCard.vue';
+import GroupDetailMembers from '~/components/groups/detail/GroupDetailMembers.vue';
 
 const route = useRoute();
 const groupId = route.params.id as string;
@@ -37,3 +38,11 @@ const {
 } = await useLazyBackendApi('/Groups/{id}', { key, path: { id: groupId } });
 provide('group', readonly(group));
 </script>
+
+<style scoped lang="scss">
+@use '~/assets/styles/utilities';
+
+main {
+  @include utilities.flex-column;
+}
+</style>
