5908e1255a2027853e9aeca5a86b4edfdf56e116
Kristof Buts <git@kristofbuts.be>
2025-06-05 18:13:56 +0200

    group activities

---
+++ frontend/app/components/groups/activities/expenses/GroupExpense.vue
@@ -0,0 +1,18 @@
+<template>
+  <AppCard>
+    <h3>{{ expense.description }}</h3>
+    <PreformattedText :value="expense" />
+  </AppCard>
+</template>
+
+<script setup lang="ts">
+import AppCard from '~/components/common/AppCard.vue';
+import type { Expense } from '~/types';
+import PreformattedText from '~/components/common/PreformattedText.vue';
+
+defineProps<{
+  expense: Expense;
+}>();
+</script>
+
+<style scoped lang="scss"></style>
--- frontend/app/pages/groups/[id]/activity.vue
+++ frontend/app/pages/groups/[id]/activity.vue
@@ -1,18 +1,24 @@
 <template>
-  <main v-if="group">
-    <h1>activity</h1>
-    <div class="expenses">
-      <h2>Expenses</h2>
-      <PreformattedText :value="expenses" />
-    </div>
-    <div class="payments">
-      <h2>Payments</h2>
-      <PreformattedText :value="payments" />
-    </div>
+  <main
+    v-if="group"
+    class="activity"
+  >
+    <h1>Group activity</h1>
+    <h2>Expenses</h2>
+    <GroupExpense
+      v-for="expense in expenses"
+      :key="expense.id"
+      :expense="expense"
+    />
+    <h2>Payments</h2>
+    <PreformattedText :value="payments" />
+    <AppCard />
   </main>
 </template>
 
 <script setup lang="ts">
+import AppCard from '~/components/common/AppCard.vue';
+import GroupExpense from '~/components/groups/activities/expenses/GroupExpense.vue';
 import PreformattedText from '~/components/common/PreformattedText.vue';
 
 const { group } = useDetailPageGroup();
@@ -20,4 +26,10 @@ const expenses = computed(() => group.value?.expenses || []);
 const payments = computed(() => group.value?.payments || []);
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@use '~/assets/styles/utilities';
+
+.activity {
+  @include utilities.flex-column;
+}
+</style>
