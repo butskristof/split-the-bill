bf9542122555212fba66c90ad282fe50d001b939
Kristof Buts <git@kristofbuts.be>
2025-06-17 22:09:31 +0200

    app host setup

--- api/src/3-hosts/AppHost/AppHost.cs
+++ api/src/3-hosts/AppHost/AppHost.cs
@@ -1,3 +1,27 @@
+using AppHost.Constants;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+#region Database
+
+var postgres = builder
+    .AddPostgres(Resources.Postgres)
+    .WithPgAdmin()
+    .WithDataVolume()
+    .WithLifetime(ContainerLifetime.Persistent);
+var db = postgres.AddDatabase(Resources.Database);
+
+#endregion
+
+#region API
+
+var api = builder.AddProject<Projects.Api>(Resources.Api).WithReference(db).WaitFor(db);
+
+#endregion
+
+#region Frontend
+
+
+#endregion
 
 builder.Build().Run();
---
+++ api/src/3-hosts/AppHost/Constants/Resources.cs
@@ -0,0 +1,9 @@
+namespace AppHost.Constants;
+
+internal static class Resources
+{
+    internal const string Postgres = "postgres";
+    internal const string Database = "postgres-database";
+
+    internal const string Api = "api";
+}
