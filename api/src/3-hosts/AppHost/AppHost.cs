e01e1544f5c934d7588fdbd22e31d830c10c077f
Kristof Buts <git@kristofbuts.be>
2025-06-18 09:44:02 +0200

    Add Aspire AppHost project

---
+++ api/src/3-hosts/AppHost/AppHost.cs
@@ -0,0 +1,18 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+#region Database
+
+
+#endregion
+
+#region API
+
+
+#endregion
+
+#region Frontend
+
+
+#endregion
+
+builder.Build().Run();
