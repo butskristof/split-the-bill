050f9e9ee9e6b293c9da21ac3efb376329161779
Kristof Buts <git@kristofbuts.be>
2025-06-21 11:52:05 +0200

    set Nuxt environment variables from Aspire AppHost

--- api/src/3-hosts/AppHost/AppHost.cs
+++ api/src/3-hosts/AppHost/AppHost.cs
@@ -1,3 +1,4 @@
+using SplitTheBill.AppHost.Constants;
 using SplitTheBill.ServiceDefaults.Constants;
 
 var builder = DistributedApplication.CreateBuilder(args);
@@ -38,12 +39,77 @@ var frontend = builder
     .WithReference(api)
     .WaitFor(api);
 
+// secret parameters
+var oidcClientId = builder.AddParameter(FrontendEnvironment.OidcClientId, secret: true);
+var oidcClientSecret = builder.AddParameter(FrontendEnvironment.OidcClientSecret, secret: true);
+var oidcTokenKey = builder.AddParameter(FrontendEnvironment.OidcTokenKey, secret: true);
+var oidcSessionSecret = builder.AddParameter(FrontendEnvironment.OidcSessionSecret, secret: true);
+var oidcAuthSessionSecret = builder.AddParameter(
+    FrontendEnvironment.OidcAuthSessionSecret,
+    secret: true
+);
+
+// non-secret parameters
+var oidcOpenIdConfig = builder.AddParameter(FrontendEnvironment.OidcOpenIdConfiguration);
+var oidcAuthUrl = builder.AddParameter(FrontendEnvironment.OidcAuthorizationUrl);
+var oidcTokenUrl = builder.AddParameter(FrontendEnvironment.OidcTokenUrl);
+var oidcUserInfoUrl = builder.AddParameter(FrontendEnvironment.OidcUserInfoUrl);
+var oidcLogoutUrl = builder.AddParameter(FrontendEnvironment.OidcLogoutUrl);
+
 frontend
+    // Dynamic service discovery URLs
     // use http to avoid certificate issues in dev mode
-    .WithEnvironment("NUXT_BACKEND_BASE_URL", api.GetEndpoint("http"))
     .WithEnvironment(
-        "NUXT_OIDC_PROVIDERS_OIDC_REDIRECT_URI",
-        () => $"{frontend.GetEndpoint("http")}/auth/oidc/callback"
+        FrontendEnvironment.EnvironmentVariables[FrontendEnvironment.BackendBaseUrl],
+        api.GetEndpoint("http")
+    )
+    .WithEnvironment(
+        FrontendEnvironment.EnvironmentVariables[FrontendEnvironment.OidcRedirectUri],
+        () =>
+        {
+            var endpoint = frontend.GetEndpoint("http");
+            return $"{endpoint.Url}/auth/oidc/callback";
+        }
+    )
+    .WithEnvironment(
+        FrontendEnvironment.EnvironmentVariables[FrontendEnvironment.OidcClientId],
+        oidcClientId
+    )
+    .WithEnvironment(
+        FrontendEnvironment.EnvironmentVariables[FrontendEnvironment.OidcClientSecret],
+        oidcClientSecret
+    )
+    .WithEnvironment(
+        FrontendEnvironment.EnvironmentVariables[FrontendEnvironment.OidcOpenIdConfiguration],
+        oidcOpenIdConfig
+    )
+    .WithEnvironment(
+        FrontendEnvironment.EnvironmentVariables[FrontendEnvironment.OidcAuthorizationUrl],
+        oidcAuthUrl
+    )
+    .WithEnvironment(
+        FrontendEnvironment.EnvironmentVariables[FrontendEnvironment.OidcTokenUrl],
+        oidcTokenUrl
+    )
+    .WithEnvironment(
+        FrontendEnvironment.EnvironmentVariables[FrontendEnvironment.OidcUserInfoUrl],
+        oidcUserInfoUrl
+    )
+    .WithEnvironment(
+        FrontendEnvironment.EnvironmentVariables[FrontendEnvironment.OidcLogoutUrl],
+        oidcLogoutUrl
+    )
+    .WithEnvironment(
+        FrontendEnvironment.EnvironmentVariables[FrontendEnvironment.OidcTokenKey],
+        oidcTokenKey
+    )
+    .WithEnvironment(
+        FrontendEnvironment.EnvironmentVariables[FrontendEnvironment.OidcSessionSecret],
+        oidcSessionSecret
+    )
+    .WithEnvironment(
+        FrontendEnvironment.EnvironmentVariables[FrontendEnvironment.OidcAuthSessionSecret],
+        oidcAuthSessionSecret
     );
 
 #endregion
