c9520cc4b24dcc1ed58bafd32382e27108a68c16
Kristof Buts <git@kristofbuts.be>
2025-10-04 11:07:45 +0200

    css reset, nuxt security module

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts'],
+  modules: ['@nuxt/eslint', '@nuxt/fonts', 'nuxt-security'],
+  security: {},
+  components: {
+    // disable auto-import of components
+    dirs: [],
+  },
+  css: ['~/styles/reset.css'],
 });
