29a10f3ba003ddc5a5c6aed92bed414f6816d578
Kristof Buts <git@kristofbuts.be>
2025-10-04 11:02:00 +0200

    env setup

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,17 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
+import pluginVueScopedCss from 'eslint-plugin-vue-scoped-css';
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt(...pluginVueScopedCss.configs['flat/recommended'], {
+  name: 'app/custom-rules',
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['../*'],
+      },
+    ],
+  },
+});
